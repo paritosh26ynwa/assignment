a270d313a050f8dbd7362de11b91ea62f2d4a2cd
Paritosh Pujari <paritosh.pujari@provakil.com>
2023-05-22 21:04:22 +0530

    adds mockapi

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,57 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+import { conversation, currentUser } from '@/mockapi';
+
+const fetchUserDetails = () => new Promise((resolve) => {
+  resolve(JSON.parse(currentUser));
+});
+
+const fetchConversations = () => new Promise((resolve) => {
+  resolve(JSON.parse(conversation));
+});
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    user: {},
+    messages: [],
   },
   getters: {
+    getUser(context) {
+      return context.user;
+    },
+    getConversation(context) {
+      return context.messages;
+    },
   },
   mutations: {
+    SET_CURRENT_USER(state, payload) {
+      state.user = payload;
+    },
+    SET_CONVERSATION(state, payload) {
+      state.messages = payload;
+    },
   },
   actions: {
-  },
-  modules: {
+    async setUserDetails({ commit }) {
+      try {
+        const user = await fetchUserDetails();
+        commit('SET_CURRENT_USER', user);
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.error(error);
+      }
+    },
+    async setConversationDetails({ commit }) {
+      try {
+        const messages = await fetchConversations();
+        commit('SET_CONVERSATION', messages);
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.error(error);
+      }
+    },
   },
 });
